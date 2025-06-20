7ea0935f1af61b6aad633d152b6f63604fbbafdd
Marko Helin <marko@koroun.se>
2025-06-20 22:28:09 +0000

    log to file by env var

--- src/shellpoker/logger.py
+++ src/shellpoker/logger.py
@@ -4,9 +4,11 @@ import os
 def create_logger():
     logger = logging.getLogger("shellpoker")
     logger.setLevel(os.environ.get("SHELLPOKER_LOGLEVEL", "ERROR"))
-    handler = logging.FileHandler("shellpoker.log", encoding="utf-8")
-    formatter = logging.Formatter('%(asctime)s %(levelname)s:%(name)s:%(message)s')
-    handler.setFormatter(formatter)
-    if not logger.hasHandlers():
-        logger.addHandler(handler)
+    log_enabled = os.environ.get("SHELLPOKER_LOG_TO_FILE", "false").lower() == "true"
+    if log_enabled:
+        handler = logging.FileHandler("shellpoker.log", encoding="utf-8")
+        formatter = logging.Formatter('%(asctime)s %(levelname)s:%(name)s:%(message)s')
+        handler.setFormatter(formatter)
+        if not logger.hasHandlers():
+            logger.addHandler(handler)
     return logger
